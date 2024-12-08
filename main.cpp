149bbbacd75e37ec9d9f84d47e09bf6500f296b6
LE2B <k023g0039`g.neec.ac.jp>
2024-12-08 22:15:02 +0900

    修正

--- main.cpp
+++ main.cpp
@@ -963,7 +963,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	//ウィンドウの生成
 	HWND hwnd = CreateWindow(
 		wc.lpszClassName,				//利用するクラス名
-		L"CG2",							//タイトルバーの文字
+		L"LE2B_08_キクチ_ケンタ",							//タイトルバーの文字
 		WS_OVERLAPPEDWINDOW,			//よく見るウィンドウスタイル
 		CW_USEDEFAULT,					//表示X座標
 		CW_USEDEFAULT,					//表示Y座標
